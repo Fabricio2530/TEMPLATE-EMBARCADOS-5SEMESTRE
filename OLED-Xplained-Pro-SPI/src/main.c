324433cd8a2edd033d5da286bf0fdd0c798e3863
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2022-03-19 21:25:23 -0300

    initial commit

---
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -0,0 +1,66 @@
+#include <asf.h>
+
+#include "PIO_OLED.h"
+#include "PIO_FUNCTIONS.h"
+#include "TC-RTT-RTC.h"
+
+#include "gfx_mono_ug_2832hsweg04.h"
+#include "gfx_mono_text.h"
+#include "sysfont.h"
+
+
+//Variaveis globais
+
+
+void init(void) {
+	//Initialize the board clock
+	sysclk_init();
+	oled_init();
+	gfx_mono_ssd1306_init();
+	
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	
+	
+}
+
+int main (void)
+{
+	board_init();
+	sysclk_init();
+	init();
+	delay_init();
+
+	gfx_mono_draw_string("Aguardando", 0,16, &sysfont);
+  /* Insert application code here, after the board has been initialized. */
+	while(1) {
+			
+			if (!pio_get(BUT_PI1,PIO_INPUT, BUT_PI1_IDX_MASK)) {
+					
+				pin_toggle(LED_PI1, LED_PI1_IDX_MASK);
+				delay_ms(300);
+				pin_toggle(LED_PI1, LED_PI1_IDX_MASK);
+				gfx_mono_draw_string("Botao 1   ", 0,16, &sysfont);
+			}
+				
+			if (!pio_get(BUT_PI2,PIO_INPUT, BUT_PI2_IDX_MASK)) {
+				
+				pin_toggle(LED_PI2, LED_PI2_IDX_MASK);
+				delay_ms(300);
+				pin_toggle(LED_PI2, LED_PI2_IDX_MASK);
+				gfx_mono_draw_string("Botao 2   ", 0,16, &sysfont);
+			}
+			
+			if (!pio_get(BUT_PI3,PIO_INPUT, BUT_PI3_IDX_MASK)) {
+				
+				pin_toggle(LED_PI3, LED_PI3_IDX_MASK);
+				delay_ms(300);
+				pin_toggle(LED_PI3, LED_PI3_IDX_MASK);
+				
+				gfx_mono_draw_string("Botao 3    ", 0,16, &sysfont);
+			}
+			
+			
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Resumo :
+
+Esse reposítorio serve como base para as futuras atividades (provas, laboratórios, APS e projetos) de Sistemas Embarcados (5° Semestre). Até o momento, ela contem o conteúdo até o 5 LAB (TC, RTC, RTT).
+
+## Como Usar
+
+Todo o conteúdo está dividido em arquivos. Os ".h" possuem o prototype e os ".c" possuem as respectivas funções. Assim, a arquitetura seguida é: .c -> .h -> main.c
+
+OBS: Os handlers do TC não estão criados, os botões ainda não seguem o padrão de interrupção.
+
+Quer utilizar esse repositório também? Dê um fork!! :)
+
+
