1b1f47f14a2fc2c56ed37362073a695ef4b9de80
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2022-03-19 21:54:29 -0300

    ajustando os prototypes do TC-RTT-RTC

--- OLED-Xplained-Pro-SPI/src/TC-RTT-RTC.h
+++ OLED-Xplained-Pro-SPI/src/TC-RTT-RTC.h
@@ -5,3 +5,9 @@ static void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSou
 
 void RTT_Handler(void);
 
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
+
+void RTC_init(Rtc *rtc, uint32_t id_rtc, calendar t, uint32_t irq_type);
+
+void RTC_Handler(void);
+
